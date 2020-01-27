dbaeeea3ce5aa1de22ed90fd1a8df18d92bd1383
Sarah Paz <sarah.paz@gmail.com>
2020-01-26 22:00:09 -0500

    complete with for loop

---
+++ javascript/calculate_parity_bit.js
@@ -0,0 +1,19 @@
+function checkParity(parity, bin) {
+  let count = 0;
+
+  for (let i = 0; i < bin.length; i++) {
+    if (bin.charAt(i) === "1") {
+      count++;
+    }
+  }
+
+  if (count % 2 === 0 && parity == "even") {
+    return 0;
+  } else if (count % 2 === 0 && parity == "odd") {
+    return 1;
+  } else if (!count % 2 === 0 && parity == "odd") {
+    return 0;
+  } else {
+    return 1;
+  }
+}
