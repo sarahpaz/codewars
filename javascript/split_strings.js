5a36acbb82a8669f3070072f4fe48242d89ca49e
Sarah Paz <sarah.paz@gmail.com>
2020-02-26 21:18:36 -0500

    change regex for solution

--- javascript/split_strings.js
+++ javascript/split_strings.js
@@ -2,10 +2,12 @@
 // https://www.codewars.com/kata/515de9ae9dcfc28eb6000001
 function solution(s) {
   if (s.length % 2 === 0) {
-    return s.match(/.{1,2}/g);
+    return s.match(/../g);
   } else {
-    const arr = s.split("");
-    arr.push("_");
-    return arr.join("").match(/.{1,2}/g);
+    const arg = s + "_";
+    return arg
+      .split("")
+      .join("")
+      .match(/../g);
   }
 }
