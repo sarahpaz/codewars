53a683d473efa39d3e802738443ca90cf8108fbb
Sarah Paz <sarahpaz@Sarahs-iMac.local>
2021-09-08 20:30:44 -0400

    add new code challenges

--- javascript/bobs_reversing_obfuscator.js
+++ javascript/bobs_reversing_obfuscator.js
@@ -1,22 +1,22 @@
 // Bob's reversing obfuscator
 //https://www.codewars.com/kata/559ee79ab98119dd0100001d/
 function decoder(encoded, marker) {
-  let start = [];
-  let end = [];
-  let findMarker = encoded.split(marker);
+	let start = [];
+	let end = [];
+	let findMarker = encoded.split(marker);
 
-  for (let i = 0; i < findMarker.length; i++) {
-    if (i % 2 !== 0) {
-      end.unshift(
-        findMarker[i]
-          .split("")
-          .reverse()
-          .join("")
-      );
-    } else {
-      start.push(findMarker[i]);
-    }
-  }
-  let whole = [...start, ...end];
-  return whole.join("");
+	for (let i = 0; i < findMarker.length; i++) {
+		if (i % 2 !== 0) {
+		end.unshift(
+			findMarker[i]
+			.split("")
+			.reverse()
+			.join("")
+		);
+		} else {
+		start.push(findMarker[i]);
+		}
+	}
+	let whole = [...start, ...end];
+	return whole.join("");
 }
--- javascript/generate_range.js
+++ javascript/generate_range.js
@@ -2,9 +2,9 @@
 // https://www.codewars.com/kata/55eca815d0d20962e1000106
 
 function generateRange(min, max, step) {
-  const arr = [];
-  for (let i = min; i <= max; i += step) {
-    arr.push(i);
-  }
-  return arr;
+	const arr = [];
+	for (let i = min; i <= max; i += step) {
+		arr.push(i);
+	}
+	return arr;
 }
