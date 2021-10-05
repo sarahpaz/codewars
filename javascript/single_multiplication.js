20e6a7af21ea9cf515e433943edc985e78d20b6d
sarahpaz <sarah.paz@gmail.com>
2021-10-04 22:11:59 -0400

    solution

---
+++ javascript/single_multiplication.js
@@ -0,0 +1,11 @@
+// single multiplication
+// https://www.codewars.com/kata/583710ccaa6717322c000105/solutions/javascript
+
+function simpleMultiplication(number) {
+    if (number % 2 == 0) {
+      return number * 8
+    } else {
+      return number * 9
+    }
+}
+
